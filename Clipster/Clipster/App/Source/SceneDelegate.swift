53f8273eebe83652967e19d3ef779b8d537f8bc5
youseokhwan <youseokhwan15@gmail.com>
2025-05-31 14:08:47 +0900

    chore: 디렉터리 분류

--- Clipster/Clipster/App/Source/SceneDelegate.swift
+++ Clipster/Clipster/App/Source/SceneDelegate.swift
@@ -10,7 +10,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 }
