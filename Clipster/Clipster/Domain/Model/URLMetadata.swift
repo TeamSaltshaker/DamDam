d886f52f2006705efc888f9fb570e2e739466af3
youseokhwan <youseokhwan15@gmail.com>
2025-06-02 13:22:15 +0900

    feat: Domain Enitity Schema 정의

---
+++ Clipster/Clipster/Domain/Model/URLMetadata.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct URLMetadata {
+    let url: URL
+    let title: String
+    let body: String
+    let thumbnailImageURL: URL
+    let createdAt: Date
+    let udpatedAt: Date
+    let deletedAt: Date?
+}
