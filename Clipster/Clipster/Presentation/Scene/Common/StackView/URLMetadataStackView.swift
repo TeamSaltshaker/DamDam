c11f97dd5017b1325abccefd0e71c4f3c9fe637f
dbguswls030 <dbguswls030@gmail.com>
2025-06-05 10:33:27 +0900

    feat: URLMetadataStackView 구현

---
+++ Clipster/Clipster/Presentation/Scene/Common/StackView/URLMetadataStackView.swift
@@ -0,0 +1,94 @@
+import Kingfisher
+import SnapKit
+import UIKit
+
+enum URLMetadataType {
+    case edit
+    case detail
+}
+
+final class URLMetadataStackView: UIStackView {
+    let type: URLMetadataType
+
+    private let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 12
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    private let infoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        return stackView
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.numberOfLines = 2
+        label.textColor = .label
+        return label
+    }()
+
+    private lazy var linkLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .medium)
+        label.numberOfLines = 2
+        label.textColor = .link
+        if case .edit = type { label.isHidden = true }
+        return label
+    }()
+
+    init(type: URLMetadataType) {
+        self.type = type
+        super.init(frame: .zero)
+        configure()
+    }
+
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setDisplay(model: URLMetadataDisplay) {
+        titleLabel.text = model.title
+        linkLabel.text = model.url.absoluteString
+        thumbnailImageView.kf.setImage(with: model.thumbnailImageURL)
+    }
+}
+
+private extension URLMetadataStackView {
+    func configure() {
+        setAttributes()
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setAttributes() {
+        axis = .horizontal
+        spacing = 12
+        distribution = .fillProportionally
+        alignment = .top
+    }
+
+    func setHierarchy() {
+        [thumbnailImageView, infoStackView].forEach {
+            addArrangedSubview($0)
+        }
+
+        [titleLabel, linkLabel].forEach {
+            infoStackView.addArrangedSubview($0)
+        }
+    }
+
+    func setConstraints() {
+        thumbnailImageView.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.width.equalTo(thumbnailImageView.snp.height)
+        }
+    }
+}
