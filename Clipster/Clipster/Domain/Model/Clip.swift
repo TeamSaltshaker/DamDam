2f9a49ad4cccdfacef71f9d1aad21337770d42f4
youseokhwan <youseokhwan15@gmail.com>
2025-06-05 19:39:54 +0900

    feat: Clip의 isVisited를 lastVisitedAt으로 변경

--- Clipster/Clipster/Domain/Model/Clip.swift
+++ Clipster/Clipster/Domain/Model/Clip.swift
@@ -5,7 +5,7 @@ struct Clip {
     let folderID: UUID
     let urlMetadata: URLMetadata
     let memo: String
-    let isVisited: Bool
+    let lastVisitedAt: Date?
     let createdAt: Date
     let updatedAt: Date
     let deletedAt: Date?
