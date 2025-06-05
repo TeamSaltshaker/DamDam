ffac648854657b65c52d79431ca5a32e3ff5ea5f
gnoes-ios <park_juseong@naver.com>
2025-06-05 20:42:52 +0900

    feat: NavigationBarItem 추가

--- Clipster/Clipster/Presentation/Scene/Home/ViewController/HomeViewController.swift
+++ Clipster/Clipster/Presentation/Scene/Home/ViewController/HomeViewController.swift
@@ -6,4 +6,56 @@ final class HomeViewController: UIViewController {
     override func loadView() {
         view = homeView
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+
+    private func makeAddButtonMenu() -> UIMenu {
+        let addFolderAction = UIAction(
+            title: "폴더 추가",
+            image: UIImage(systemName: "folder")
+        ) { _ in
+            print("폴더 추가 버튼 탭")
+        }
+
+        let addClipAction = UIAction(
+            title: "클립 추가",
+            image: UIImage(systemName: "paperclip"),
+        ) { _ in
+            print("클립 추가 버튼 탭")
+        }
+
+        return UIMenu(title: "", children: [addFolderAction, addClipAction])
+    }
+}
+
+private extension HomeViewController {
+    func configure() {
+        setAttributes()
+        setNavigationBarItems()
+    }
+
+    func setAttributes() {
+        title = "Clipster"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    func setNavigationBarItems() {
+        let infoButton = UIBarButtonItem(
+            image: UIImage(systemName: "info.circle"),
+            primaryAction: UIAction { _ in
+                print("라이센스 버튼 탭")
+            }
+        )
+
+        let addButton = UIBarButtonItem(
+            systemItem: .add,
+            menu: makeAddButtonMenu()
+        )
+
+        navigationController?.navigationBar.tintColor = .label
+        navigationItem.rightBarButtonItems = [infoButton, addButton]
+    }
 }
