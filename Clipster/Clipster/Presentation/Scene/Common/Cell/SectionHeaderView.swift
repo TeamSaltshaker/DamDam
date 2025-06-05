b30bbb787f7c59082d3dd6bb00b7de2a50ce75e0
gnoes-ios <park_juseong@naver.com>
2025-06-05 20:38:23 +0900

    feat: SectionHeaderView UI 구현

---
+++ Clipster/Clipster/Presentation/Scene/Common/Cell/SectionHeaderView.swift
@@ -0,0 +1,41 @@
+import SnapKit
+import UIKit
+
+final class SectionHeaderView: UICollectionReusableView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
+}
+
+private extension SectionHeaderView {
+    func configure() {
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setHierarchy() {
+        addSubview(titleLabel)
+    }
+
+    func setConstraints() {
+        titleLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(12)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
