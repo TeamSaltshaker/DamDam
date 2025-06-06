712a6e99f01506baab1a560ae59bedb6ba87fe52
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-06 17:51:50 +0900

    feat: FetchClipUseCase 구조 정의

--- Clipster/Clipster/Domain/UseCase/Clip/DefaultFetchClipUseCase.swift
+++ Clipster/Clipster/Domain/UseCase/Clip/DefaultFetchClipUseCase.swift
@@ -1,1 +1,7 @@
-final class DefaultFetchClipUseCase: FetchClipUseCase {}
+import Foundation
+
+final class DefaultFetchClipUseCase: FetchClipUseCase {
+    func execute(id: UUID) async -> Result<Clip, any Error> {
+        .failure(DummyError.notImplemented)
+    }
+}
