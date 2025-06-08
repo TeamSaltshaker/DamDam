c47e3100eb3835b942fafa53cdee2ec47fe03ee6
gnoes-ios <park_juseong@naver.com>
2025-06-08 21:38:03 +0900

    feat: SectionHeader View 전체보기 버튼 추가

--- Clipster/Clipster/Presentation/Scene/Common/Cell/SectionHeaderView.swift
+++ Clipster/Clipster/Presentation/Scene/Common/Cell/SectionHeaderView.swift
@@ -1,11 +1,26 @@
+import RxRelay
+import RxSwift
 import SnapKit
 import UIKit
 
 final class SectionHeaderView: UICollectionReusableView {
+    let showAllTapped = PublishRelay<Void>()
+    private let disposeBag = DisposeBag()
+
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16, weight: .semibold)
         return label
+    }()
+
+    private let showAllButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체보기", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        button.titleLabel?.textAlignment = .center
+        button.isHidden = true
+        return button
     }()
 
     override init(frame: CGRect) {
@@ -20,21 +35,42 @@ final class SectionHeaderView: UICollectionReusableView {
     func setTitle(_ title: String) {
         titleLabel.text = title
     }
+
+    func setShowAllButtonVisible(_ isVisible: Bool) {
+        showAllButton.isHidden = !isVisible
+    }
 }
 
 private extension SectionHeaderView {
     func configure() {
         setHierarchy()
         setConstraints()
+        setBindings()
     }
 
     func setHierarchy() {
-        addSubview(titleLabel)
+        [
+            titleLabel,
+            showAllButton
+        ].forEach { addSubview($0) }
     }
 
     func setConstraints() {
-        titleLabel.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+        titleLabel.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview()
+            make.leading.equalToSuperview()
         }
+
+        showAllButton.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview()
+            make.leading.greaterThanOrEqualTo(titleLabel).offset(12)
+            make.trailing.equalToSuperview()
+        }
+    }
+
+    func setBindings() {
+        showAllButton.rx.tap
+            .bind(to: showAllTapped)
+            .disposed(by: disposeBag)
     }
 }
