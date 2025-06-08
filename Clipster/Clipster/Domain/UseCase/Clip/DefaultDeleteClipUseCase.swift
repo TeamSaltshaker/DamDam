6225b35bafd7fda1b09f17697b99814df07d2367
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-08 16:33:52 +0900

    feat: 클립 삭제 UseCase 구현

--- Clipster/Clipster/Domain/UseCase/Clip/DefaultDeleteClipUseCase.swift
+++ Clipster/Clipster/Domain/UseCase/Clip/DefaultDeleteClipUseCase.swift
@@ -1,5 +1,11 @@
 final class DefaultDeleteClipUseCase: DeleteClipUseCase {
+    private let clipRepository: ClipRepository
+
+     init(clipRepository: ClipRepository) {
+         self.clipRepository = clipRepository
+     }
+
     func execute(_ clip: Clip) async -> Result<Void, Error> {
-        .success(())
+        clipRepository.deleteClip(clip).mapError { $0 as Error }
     }
 }
