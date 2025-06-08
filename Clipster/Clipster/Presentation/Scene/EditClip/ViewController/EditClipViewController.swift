c9cea06d9f1282bcb3fd12078ae2e16ccfc0902a
dbguswls030 <dbguswls030@gmail.com>
2025-06-08 19:25:07 +0900

    feat: 키보드 내리기 Extension 추가

--- Clipster/Clipster/Presentation/Scene/EditClip/ViewController/EditClipViewController.swift
+++ Clipster/Clipster/Presentation/Scene/EditClip/ViewController/EditClipViewController.swift
@@ -24,6 +24,7 @@ final class EditClipViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        hideKeyboardWhenTappedBackground()
     }
 }
 
