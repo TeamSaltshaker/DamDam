7b9c4a1a8f6263d46f347d227be4964dba16d3c5
youseokhwan <youseokhwan15@gmail.com>
2025-06-08 20:41:59 +0900

    feat: FolderVM Action, State, Navigation 설계

--- Clipster/Clipster/Presentation/Scene/Folder/ViewModel/FolderViewModel.swift
+++ Clipster/Clipster/Presentation/Scene/Folder/ViewModel/FolderViewModel.swift
@@ -1,3 +1,55 @@
 import Foundation
+import RxSwift
+import RxRelay
 
-final class FolderViewModel {}
+final class FolderViewModel {
+    enum Action {
+        case didTapAddClipButton
+        case didTapAddFolderButton
+        case didTapCell(IndexPath)
+        case didTapEditButton(IndexPath)
+        case didTapDeleteButton(IndexPath)
+    }
+
+    struct State {
+        let currentFolderTitle: String
+        let folders: [FolderCellDisplay]
+        let clips: [ClipCellDisplay]
+    }
+
+    enum Navigation {
+        case editClipView(Clip?)
+        case editFolderView(Folder?)
+        case folderView(Folder)
+        case clipDetailView(Clip)
+        case webView(URL)
+    }
+
+    var action = PublishRelay<Action>()
+    var state: BehaviorRelay<State>
+    var navigation = PublishRelay<Navigation>()
+    var disposeBag = DisposeBag()
+
+    private let folder: Folder
+    private let mapper: CellDisplayMapper
+
+    init(
+        folder: Folder,
+        mapper: CellDisplayMapper,
+    ) {
+        self.folder = folder
+        self.mapper = mapper
+        state = BehaviorRelay(value: State(
+            currentFolderTitle: folder.title,
+            folders: folder.folders.map { mapper.folderCellDisplay(from: $0) },
+            clips: folder.clips.map { mapper.clipCellDisplay(from: $0) },
+        ))
+        setBindings()
+    }
+}
+
+private extension FolderViewModel {
+    func setBindings() {
+
+    }
+}
