3fc41390260654e19d949c5b90443f712aa13f12
youseokhwan <youseokhwan15@gmail.com>
2025-06-08 21:14:37 +0900

    feat: action에 대한 로직 구현

--- Clipster/Clipster/Presentation/Scene/Folder/ViewModel/FolderViewModel.swift
+++ Clipster/Clipster/Presentation/Scene/Folder/ViewModel/FolderViewModel.swift
@@ -1,12 +1,12 @@
 import Foundation
-import RxSwift
 import RxRelay
+import RxSwift
 
 final class FolderViewModel {
     enum Action {
-        case didTapAddClipButton
-        case didTapAddFolderButton
         case didTapCell(IndexPath)
+        case didTapAddFolderButton
+        case didTapAddClipButton
         case didTapEditButton(IndexPath)
         case didTapDeleteButton(IndexPath)
     }
@@ -32,13 +32,20 @@ final class FolderViewModel {
 
     private let folder: Folder
     private let mapper: CellDisplayMapper
+    private let deleteFolderUseCase: DeleteFolderUseCase
+    private let deleteClipUseCase: DeleteClipUseCase
 
     init(
         folder: Folder,
         mapper: CellDisplayMapper,
+        deleteFolderUseCase: DeleteFolderUseCase,
+        deleteClipUseCase: DeleteClipUseCase,
     ) {
         self.folder = folder
         self.mapper = mapper
+        self.deleteFolderUseCase = deleteFolderUseCase
+        self.deleteClipUseCase = deleteClipUseCase
+
         state = BehaviorRelay(value: State(
             currentFolderTitle: folder.title,
             folders: folder.folders.map { mapper.folderCellDisplay(from: $0) },
@@ -50,6 +57,79 @@ final class FolderViewModel {
 
 private extension FolderViewModel {
     func setBindings() {
+        action
+            .subscribe { [weak self] action in
+                guard let self else { return }
+                print("\(Self.self): \(action)")
 
+                switch action {
+                case .didTapCell(let indexPath):
+                    switch indexPath.section {
+                    case 0:
+                        navigateToFolderView(at: indexPath.item)
+                    case 1:
+                        navigateToWebView(at: indexPath.item)
+                    default:
+                        break
+                    }
+                case .didTapAddClipButton:
+                    navigateToAddClipView()
+                case .didTapAddFolderButton:
+                    navigateToAddFolderView()
+                case .didTapEditButton(let indexPath):
+                    navigateToEditView(at: indexPath)
+                case .didTapDeleteButton(let indexPath):
+                    delete(at: indexPath)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+
+    func navigateToWebView(at index: Int) {
+        let url = folder.clips[index].urlMetadata.url
+        navigation.accept(.webView(url))
+    }
+
+    func navigateToFolderView(at index: Int) {
+        let folder = folder.folders[index]
+        navigation.accept(.folderView(folder))
+    }
+
+    func navigateToAddClipView() {
+        navigation.accept(.editClipView(nil))
+    }
+
+    func navigateToAddFolderView() {
+        navigation.accept(.editFolderView(nil))
+    }
+
+    func navigateToEditView(at indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            let folder = folder.folders[indexPath.item]
+            navigation.accept(.editFolderView(folder))
+        case 1:
+            let clip = folder.clips[indexPath.item]
+            navigation.accept(.editClipView(clip))
+        default:
+            break
+        }
+    }
+
+    func delete(at indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            Task {
+                let folder = folder.folders[indexPath.item]
+                _ = await deleteFolderUseCase.execute(folder)
+            }
+        case 1:
+            Task {
+                let clip = folder.clips[indexPath.item]
+                _ = await deleteClipUseCase.execute(clip)
+            }
+        default:
+            break
+        }
     }
 }
