dd2e01e6361a5715b2b5f2f93a2846ca893dc56a
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-09 23:05:41 +0900

    feat: 내비게이션 바 구현

---
+++ Clipster/Clipster/Presentation/Scene/FolderSelector/Subview/View/FolderSelectorNavigationView.swift
@@ -0,0 +1,73 @@
+import SnapKit
+import UIKit
+
+final class FolderSelectorNavigationView: UIView {
+    private let backButton: BackButton = {
+        let button = BackButton()
+        button.setDisplay("이전폴더")
+        return button
+    }()
+
+    private let titleView = UIView()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.textColor = .label
+        return label
+    }()
+
+    private let selectButton = SelectButton()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func setDisplay(_ folder: FolderDisplay) {
+        titleLabel.text = folder.title
+    }
+}
+
+private extension FolderSelectorNavigationView {
+    func configure() {
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setHierarchy() {
+        [backButton, titleView, titleLabel, selectButton]
+            .forEach { addSubview($0) }
+    }
+
+    func setConstraints() {
+        backButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24)
+            make.centerY.equalToSuperview()
+        }
+        backButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+        titleView.snp.makeConstraints { make in
+            make.leading.equalTo(backButton.snp.trailing).offset(8)
+            make.trailing.equalTo(selectButton.snp.leading).offset(-8)
+            make.top.bottom.equalToSuperview()
+        }
+
+        titleLabel.snp.makeConstraints { make in
+            make.leading.greaterThanOrEqualTo(titleView.snp.leading)
+            make.trailing.lessThanOrEqualTo(titleView.snp.trailing)
+            make.centerX.equalToSuperview().priority(.high)
+            make.centerY.equalToSuperview()
+        }
+
+        selectButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(24)
+            make.centerY.equalToSuperview()
+        }
+        selectButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+    }
+}
