54e322c5503acb63b29389ae309a71e104f72c50
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-09 23:56:48 +0900

    feat: 액션 정의

--- Clipster/Clipster/Presentation/Scene/FolderSelector/ViewModel/FolderSelectorViewModel.swift
+++ Clipster/Clipster/Presentation/Scene/FolderSelector/ViewModel/FolderSelectorViewModel.swift
@@ -2,3 +2,14 @@ enum FolderSelectorMode {
     case clip
     case folder
 }
+
+enum FolderSelectorAction {
+    case viewDidLoad
+    case dataLoaded(folders: [Folder])
+    case dataLoadFailed(Error)
+    case openSubfolder(folder: Folder)
+    case navigateUp
+    case selectButtonTapped
+    case addButtonTapped
+    case folderAdded(folder: Folder)
+}
