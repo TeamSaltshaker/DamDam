12814da5168187e297944652b1bd93fc06a28ad7
gnoes-ios <park_juseong@naver.com>
2025-06-09 17:54:25 +0900

    refactor: 의존성 주입 추가

--- Clipster/Clipster/App/Source/SceneDelegate.swift
+++ Clipster/Clipster/App/Source/SceneDelegate.swift
@@ -11,18 +11,25 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let context = CoreDataStack.shared.context
-        let storage = DefaultClipStorage(context: context)
-        let clipRepository = DefaultClipRepository(storage: storage, mapper: DomainMapper())
-        let fetchUnvisitedClipsUseCase = DefaultFetchUnvisitedClipsUseCase()
-        let fetchTopLevelFoldersUseCase = DefaultFetchTopLevelFoldersUseCase()
+
+        let clipStorage = DefaultClipStorage(context: context)
+        let folderStorage = DefaultFolderStorage(context: context)
+
+        let clipRepository = DefaultClipRepository(storage: clipStorage, mapper: DomainMapper())
+        let folderRepository = DefaultFolderRepository(storage: folderStorage, mapper: DomainMapper())
+
+        let fetchUnvisitedClipsUseCase = DefaultFetchUnvisitedClipsUseCase(clipRepository: clipRepository)
+        let fetchTopLevelFoldersUseCase = DefaultFetchTopLevelFoldersUseCase(folderRepository: folderRepository)
         let deleteClipUseCase = DefaultDeleteClipUseCase(clipRepository: clipRepository)
-        let deleteFolderUseCase = DefaultDeleteFolderUseCase()
+        let deleteFolderUseCase = DefaultDeleteFolderUseCase(folderRepository: folderRepository)
+
         let homeViewModel = HomeViewModel(
             fetchUnvisitedClipsUseCase: fetchUnvisitedClipsUseCase,
             fetchTopLevelFoldersUseCase: fetchTopLevelFoldersUseCase,
             deleteClipUseCase: deleteClipUseCase,
             deleteFolderUseCase: deleteFolderUseCase
         )
+
         let homeVC = HomeViewController(homeviewModel: homeViewModel)
         window?.rootViewController = UINavigationController(rootViewController: homeVC)
         window?.makeKeyAndVisible()
