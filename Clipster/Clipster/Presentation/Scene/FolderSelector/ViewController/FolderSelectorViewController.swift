ae19df3740e797e0c573d0e6c23b886f034aba8f
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-09 23:08:15 +0900

    feat: 폴더 선택 화면 구현

---
+++ Clipster/Clipster/Presentation/Scene/FolderSelector/ViewController/FolderSelectorViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class FolderSelectorViewController: UIViewController {
+    private let folderSelectorView = FolderSelectorView()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = folderSelectorView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
