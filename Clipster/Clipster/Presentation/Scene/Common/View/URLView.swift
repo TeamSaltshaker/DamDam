c2bd61d9eebe8158d9bdccce6459ca43ac141ee9
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-10 21:01:11 +0900

    feat: 공통 URL 뷰 구현

---
+++ Clipster/Clipster/Presentation/Scene/Common/View/URLView.swift
@@ -0,0 +1,75 @@
+import SnapKit
+import UIKit
+
+final class URLView: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "URL"
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .label
+        return label
+    }()
+
+    let urlTextField: CommonTextField = {
+        let textField = CommonTextField()
+        textField.placeholder = "URL을 입력해주세요."
+        return textField
+    }()
+
+    let statusImageView = UIImageView()
+
+    let statusLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+private extension URLView {
+    func configure() {
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setHierarchy() {
+        [titleLabel, urlTextField, statusImageView, statusLabel]
+            .forEach { addSubview($0) }
+    }
+
+    func setConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.directionalHorizontalEdges.equalToSuperview().inset(24)
+        }
+
+        urlTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.directionalHorizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(48)
+        }
+
+        statusImageView.snp.makeConstraints { make in
+            make.top.equalTo(urlTextField.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(36)
+            make.bottom.equalToSuperview().inset(16)
+            make.size.equalTo(19)
+        }
+
+        statusLabel.snp.makeConstraints { make in
+            make.top.equalTo(urlTextField.snp.bottom).offset(8)
+            make.leading.equalTo(statusImageView.snp.trailing).offset(4)
+            make.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(16)
+        }
+    }
+}
