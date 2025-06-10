9c56f24335d1f08dc233e44b09b7eaf3f32bbb1b
youseokhwan <youseokhwan15@gmail.com>
2025-06-10 20:05:07 +0900

    feat: CollectionView를 TableView로 변경

--- Clipster/Clipster/Presentation/Scene/Folder/ViewController/FolderViewController.swift
+++ Clipster/Clipster/Presentation/Scene/Folder/ViewController/FolderViewController.swift
@@ -54,7 +54,6 @@ private extension FolderViewController {
     func configure() {
         setNavigationBarItems()
         setBindings()
-        setDelegate()
     }
 
     func setNavigationBarItems() {
@@ -116,57 +115,36 @@ private extension FolderViewController {
             }
             .disposed(by: disposeBag)
 
-        folderView.collectionView
-            .rx
-            .itemSelected
+        folderView.didTapCell
             .asDriver(onErrorDriveWith: .empty())
             .drive { [weak self] indexPath in
                 guard let self else { return }
                 viewModel.action.accept(.didTapCell(indexPath))
             }
             .disposed(by: disposeBag)
-    }
 
-    func setDelegate() {
-        folderView.collectionView
-            .rx
-            .setDelegate(self)
+        folderView.didTapDetailButton
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] indexPath in
+                guard let self else { return }
+                viewModel.action.accept(.didTapDetailButton(indexPath))
+            }
             .disposed(by: disposeBag)
-    }
-}
 
-extension FolderViewController: UICollectionViewDelegate {
-    func collectionView(
-        _ collectionView: UICollectionView,
-        contextMenuConfigurationForItemsAt indexPaths: [IndexPath],
-        point: CGPoint,
-    ) -> UIContextMenuConfiguration? {
-        guard let indexPath = indexPaths.first else { return nil }
+        folderView.didTapEditButton
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] indexPath in
+                guard let self else { return }
+                viewModel.action.accept(.didTapEditButton(indexPath))
+            }
+            .disposed(by: disposeBag)
 
-        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { [weak self] _ in
-            guard let self else { return UIMenu() }
-
-            let detailAction = UIAction(
-                title: "상세정보",
-                image: UIImage(systemName: "info.circle"),
-            ) { _ in
-                self.viewModel.action.accept(.didTapDetailButton(indexPath))
+        folderView.didTapDeleteButton
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] indexPath in
+                guard let self else { return }
+                viewModel.action.accept(.didTapDeleteButton(indexPath))
             }
-            let editAction = UIAction(
-                title: "편집",
-                image: UIImage(systemName: "pencil"),
-            ) { _ in
-                self.viewModel.action.accept(.didTapEditButton(indexPath))
-            }
-            let deleteAction = UIAction(
-                title: "삭제",
-                image: UIImage(systemName: "trash"),
-                attributes: .destructive,
-            ) { _ in
-                self.viewModel.action.accept(.didTapDeleteButton(indexPath))
-            }
-            let actions = (indexPath.section == 0 ? [] : [detailAction]) + [editAction, deleteAction]
-            return UIMenu(title: "", children: actions)
-        }
+            .disposed(by: disposeBag)
     }
 }
