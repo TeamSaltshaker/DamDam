b05860825434b9ab0ee271ee1a10e6766f1c079a
dbguswls030 <dbguswls030@gmail.com>
2025-06-10 10:07:53 +0900

    feat: main app 진입 구현

--- Clipster/ShareExtension/ShareViewController.swift
+++ Clipster/ShareExtension/ShareViewController.swift
@@ -1,30 +1,32 @@
-//
-//  ShareViewController.swift
-//  ShareExtension
-//
-//  Created by 유현진 on 6/9/25.
-//
-
-import UIKit
 import Social
+import UIKit
 
-class ShareViewController: SLComposeServiceViewController {
-
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+final class ShareViewController: SLComposeViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        openMainApp()
     }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    private func openMainApp() {
+        if let url = URL(string: "clipster://") {
+            if openURL(url) {
+                print("url scheme success")
+            } else {
+                print("url scheme failed")
+            }
+        }
+        self.extensionContext?.completeRequest(returningItems: nil)
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    private func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                application.open(url, options: [:], completionHandler: nil)
+                return true
+            }
+            responder = responder?.next
+        }
+        return false
     }
-
 }
