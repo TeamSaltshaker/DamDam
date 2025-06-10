2b2c873bf66edca776203926fdf492e632245773
gnoes-ios <park_juseong@naver.com>
2025-06-10 22:05:15 +0900

    feat: TableHeaderView

---
+++ Clipster/Clipster/Presentation/Scene/Common/Cell/TableHeaderView.swift
@@ -0,0 +1,40 @@
+import SnapKit
+import UIKit
+
+final class TableHeaderView: UITableViewHeaderFooterView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+
+    override init(reuseIdentifier: String?) {
+         super.init(reuseIdentifier: reuseIdentifier)
+         configure()
+     }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setTitle(_ title: String) {
+        titleLabel.text = title
+    }
+}
+
+private extension TableHeaderView {
+    func configure() {
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setHierarchy() {
+        [titleLabel].forEach { addSubview($0) }
+    }
+
+    func setConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
