7908d9ca333772ea432ffa6eda8a39f37071a498
youseokhwan <youseokhwan15@gmail.com>
2025-06-12 15:13:35 +0900

    refactor: Storage 변경에 따른 ClipRepository 변경

--- Clipster/Clipster/Data/Repository/Clip/DefaultClipRepository.swift
+++ Clipster/Clipster/Data/Repository/Clip/DefaultClipRepository.swift
@@ -2,48 +2,33 @@ import Foundation
 
 final class DefaultClipRepository: ClipRepository {
     private let storage: ClipStorage
-    private let mapper: DomainMapper
 
-    init(
-        storage: ClipStorage,
-        mapper: DomainMapper,
-    ) {
+    init(storage: ClipStorage) {
         self.storage = storage
-        self.mapper = mapper
     }
 
-    func fetchClip(by id: UUID) -> Result<Clip, DomainError> {
-        storage.fetchClip(by: id)
-            .mapError { _ in .unknownError }
-            .flatMap { entity in
-                guard let clip = mapper.clip(from: entity) else {
-                    return .failure(.unknownError)
-                }
-                return .success(clip)
-            }
-    }
-
-    func fetchUnvisitedClips() -> Result<[Clip], DomainError> {
-        storage.fetchUnvisitedClips()
-            .mapError { _ in .unknownError }
-            .flatMap { entities in
-                let clips = entities.compactMap(mapper.clip)
-                return .success(clips)
-            }
-    }
-
-    func createClip(_ clip: Clip) -> Result<Void, DomainError> {
-        storage.insertClip(clip)
+    func fetchClip(by id: UUID) async -> Result<Clip, DomainError> {
+        await storage.fetchClip(by: id)
             .mapError { _ in .unknownError }
     }
 
-    func updateClip(_ clip: Clip) -> Result<Void, DomainError> {
-        storage.updateClip(clip)
+    func fetchUnvisitedClips() async -> Result<[Clip], DomainError> {
+        await storage.fetchUnvisitedClips()
             .mapError { _ in .unknownError }
     }
 
-    func deleteClip(_ clip: Clip) -> Result<Void, DomainError> {
-        storage.deleteClip(clip)
+    func createClip(_ clip: Clip) async -> Result<Void, DomainError> {
+        await storage.insertClip(clip)
+            .mapError { _ in .unknownError }
+    }
+
+    func updateClip(_ clip: Clip) async -> Result<Void, DomainError> {
+        await storage.updateClip(clip)
+            .mapError { _ in .unknownError }
+    }
+
+    func deleteClip(_ clip: Clip) async -> Result<Void, DomainError> {
+        await storage.deleteClip(clip)
             .mapError { _ in .unknownError }
     }
 }
