fdd56ae6bde8d9b5886107d557f714cfad213d32
gnoes-ios <park_juseong@naver.com>
2025-06-12 15:19:01 +0900

    feat: presentDeleteAlert 확장으로 재사용

--- Clipster/Clipster/Presentation/Util/Extension/ViewController+Extension.swift
+++ Clipster/Clipster/Presentation/Util/Extension/ViewController+Extension.swift
@@ -10,4 +10,27 @@ extension UIViewController {
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
+
+    func presentDeleteAlert(
+        title: String,
+        message: String = "삭제하겠습니까?",
+        onConfirm: @escaping () -> Void
+    ) {
+        let trimmedTitle = title.count > 20
+            ? String(title.prefix(20)) + "...는"
+            : title + "는"
+
+        let alert = UIAlertController(
+            title: trimmedTitle,
+            message: message,
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+        alert.addAction(UIAlertAction(title: "삭제", style: .destructive) { _ in
+            onConfirm()
+        })
+
+        present(alert, animated: true)
+    }
 }
