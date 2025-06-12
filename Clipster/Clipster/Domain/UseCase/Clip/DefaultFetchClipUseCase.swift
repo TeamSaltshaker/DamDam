963ea9128a9f76275a4049c916a9534496b86e45
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-12 19:55:42 +0900

    refactor: indent 5 space -> 4 space

--- Clipster/Clipster/Domain/UseCase/Clip/DefaultFetchClipUseCase.swift
+++ Clipster/Clipster/Domain/UseCase/Clip/DefaultFetchClipUseCase.swift
@@ -3,9 +3,9 @@ import Foundation
 final class DefaultFetchClipUseCase: FetchClipUseCase {
     private let clipRepository: ClipRepository
 
-     init(clipRepository: ClipRepository) {
-         self.clipRepository = clipRepository
-     }
+    init(clipRepository: ClipRepository) {
+        self.clipRepository = clipRepository
+    }
 
     func execute(id: UUID) async -> Result<Clip, Error> {
         await clipRepository.fetchClip(by: id).mapError { $0 as Error }
