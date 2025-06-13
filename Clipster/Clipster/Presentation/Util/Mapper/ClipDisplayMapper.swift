f5b9b47ff7343a2f378ae936ac8aa5df6847ec5a
youseokhwan <youseokhwan15@gmail.com>
2025-06-13 21:11:56 +0900

    fix: ClipCell에서 제목이나 메모가 비어있는 경우에도 자리를 차지하도록 수정

--- Clipster/Clipster/Presentation/Util/Mapper/ClipDisplayMapper.swift
+++ Clipster/Clipster/Presentation/Util/Mapper/ClipDisplayMapper.swift
@@ -4,14 +4,14 @@ struct ClipDisplayMapper {
     static func map(_ clip: Clip) -> ClipDisplay {
         let urlMetadataDisplay = URLMetadataDisplay(
             url: clip.urlMetadata.url,
-            title: clip.urlMetadata.title,
+            title: clip.urlMetadata.title.isEmpty ? " " : clip.urlMetadata.title,
             thumbnailImageURL: clip.urlMetadata.thumbnailImageURL
         )
 
         return ClipDisplay(
             id: clip.id,
             urlMetadata: urlMetadataDisplay,
-            memo: clip.memo,
+            memo: clip.memo.isEmpty ? " " : clip.memo,
             memoLimit: "\(clip.memo.count) / 100",
             isVisited: clip.lastVisitedAt != nil
         )
