28505f23f91dc5ebbaee9013322c20a3b7af28da
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-16 17:03:06 +0900

    fix: EditClip 공통 컴포넌트 수정사항 반영

--- Clipster/Clipster/Presentation/Scene/EditClip/Subview/View/EditClipView.swift
+++ Clipster/Clipster/Presentation/Scene/EditClip/Subview/View/EditClipView.swift
@@ -7,7 +7,7 @@ final class EditClipView: UIView {
     let saveButton = SaveButton()
 
     let urlMetadataStackView: URLMetadataStackView = {
-        let stackView = URLMetadataStackView(type: .edit)
+        let stackView = URLMetadataStackView()
         stackView.isHidden = true
         return stackView
     }()
@@ -27,32 +27,10 @@ final class EditClipView: UIView {
         return stackView
     }()
 
-    let urlLabelView = UIView()
-
-    let urlLabel: UILabel = {
-        let label = UILabel()
-        label.text = "URL"
-        label.textColor = .black100
-        label.font = .pretendard(size: 16, weight: .medium)
-        return label
-    }()
-
-    let urlInputTextField: CommonTextField = {
-        let textField = CommonTextField()
-        textField.keyboardType = .URL
-        textField.autocapitalizationType = .none
-        textField.autocorrectionType = .no
-        textField.spellCheckingType = .no
-        textField.smartDashesType = .no
-        textField.smartInsertDeleteType = .no
-        textField.attributedPlaceholder = NSAttributedString(
-            string: "URL을 입력해 주세요.",
-            attributes: [
-                .foregroundColor: UIColor.black800,
-                .font: UIFont.pretendard(size: 14, weight: .regular)
-            ]
-        )
-        return textField
+    let urlView: URLView = {
+        let view = URLView()
+        view.urlTextField.mode = .edit
+        return view
     }()
 
     let urlValidationStacKView: URLValidationStackView = {
@@ -63,38 +41,7 @@ final class EditClipView: UIView {
 
     let memoView = MemoView()
 
-    let folderLabel: UILabel = {
-        let label = UILabel()
-        label.text = "저장폴더"
-        label.textColor = .black100
-        label.font = .pretendard(size: 16, weight: .medium)
-        return label
-    }()
-
-    let addFolderButton = AddButton()
-
-    lazy var folderView: UIView = {
-        let view = UIView()
-        view.addGestureRecognizer(folderViewTapGesture)
-        view.isUserInteractionEnabled = true
-        view.backgroundColor = .white900
-        view.layer.cornerRadius = 12
-        view.clipsToBounds = true
-        return view
-    }()
-
-    let folderRowView = FolderRowView()
-
-    private let chevronImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = .chevronRight
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
-
-    let emptyView = EmptyView(type: .editClipView)
-
-    let folderViewTapGesture = UITapGestureRecognizer()
+    let selectedFolderView = SelectedFolderView(mode: .edit)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -122,32 +69,19 @@ private extension EditClipView {
         [
             urlStackView,
             memoView,
-            folderLabel,
-            addFolderButton,
-            folderView,
-            emptyView
+            selectedFolderView
         ].forEach {
             scrollContainerView.addSubview($0)
         }
 
         scrollView.addSubview(scrollContainerView)
 
-        urlLabelView.addSubview(urlLabel)
-
         [
             urlMetadataStackView,
-            urlLabelView,
-            urlInputTextField,
+            urlView,
             urlValidationStacKView
         ].forEach {
             urlStackView.addArrangedSubview($0)
-        }
-
-        [
-            folderRowView,
-            chevronImageView
-        ].forEach {
-            folderView.addSubview($0)
         }
 
         [
@@ -185,54 +119,15 @@ private extension EditClipView {
             make.height.equalTo(0).priority(.low)
         }
 
-        urlLabel.snp.makeConstraints { make in
-            make.verticalEdges.equalToSuperview()
-            make.directionalHorizontalEdges.equalToSuperview().offset(4)
-        }
-
-        urlInputTextField.snp.makeConstraints { make in
-            make.height.equalTo(48)
-        }
-
         memoView.snp.makeConstraints { make in
             make.top.equalTo(urlStackView.snp.bottom).offset(32)
             make.directionalHorizontalEdges.equalToSuperview().inset(24)
         }
 
-        folderLabel.snp.makeConstraints { make in
+        selectedFolderView.snp.makeConstraints { make in
             make.top.equalTo(memoView.snp.bottom).offset(40)
-            make.leading.equalToSuperview().inset(28)
-        }
-
-        addFolderButton.snp.makeConstraints { make in
-            make.centerY.equalTo(folderLabel)
-            make.trailing.equalToSuperview().inset(28)
-            make.size.equalTo(48)
-        }
-
-        folderView.snp.makeConstraints { make in
-            make.top.equalTo(folderLabel.snp.bottom).offset(24)
             make.directionalHorizontalEdges.equalToSuperview().inset(24)
             make.bottom.lessThanOrEqualToSuperview().inset(24)
-            make.height.equalTo(72)
-        }
-
-        folderRowView.snp.makeConstraints { make in
-            make.verticalEdges.equalToSuperview().inset(12)
-            make.leading.equalToSuperview().offset(20)
-            make.trailing.equalTo(chevronImageView.snp.leading).offset(-16)
-        }
-
-        chevronImageView.snp.makeConstraints { make in
-            make.trailing.equalToSuperview().inset(20)
-            make.centerY.equalToSuperview()
-            make.size.equalTo(24)
-        }
-
-        emptyView.snp.makeConstraints { make in
-            make.top.equalTo(folderLabel.snp.bottom).offset(34)
-            make.directionalHorizontalEdges.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(22)
         }
     }
 }
