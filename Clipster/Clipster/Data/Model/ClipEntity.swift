cec729c7743d1fcc019ed8b45f55da9559b2c5ef
youseokhwan <youseokhwan15@gmail.com>
2025-06-19 01:58:37 +0900

    feat: 다음 모델 버전 기준으로 스키마 수정

--- Clipster/Clipster/Data/Model/ClipEntity.swift
+++ Clipster/Clipster/Data/Model/ClipEntity.swift
@@ -3,14 +3,17 @@ import CoreData
 @objc(Clip)
 public class ClipEntity: NSManagedObject {
     @NSManaged public var id: UUID
+    @NSManaged public var urlString: String
+    @NSManaged public var title: String
     @NSManaged public var memo: String
+    @NSManaged public var thumbnailImageURLString: String?
+    @NSManaged public var screenshotData: Data?
     @NSManaged public var lastVisitedAt: Date?
     @NSManaged public var createdAt: Date
     @NSManaged public var updatedAt: Date
     @NSManaged public var deletedAt: Date?
 
     @NSManaged public var folder: FolderEntity?
-    @NSManaged public var urlMetadata: URLMetadataEntity?
 }
 
 extension ClipEntity {
