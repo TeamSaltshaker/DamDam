cb3a5d053dbf5b3a2f3a472ddffb6cbb965db314
youseokhwan <youseokhwan15@gmail.com>
2025-06-19 17:58:29 +0900

    feat: URLMetadata에 screenshotData 속성 추가

--- Clipster/Clipster/Domain/Model/URLMetadata.swift
+++ Clipster/Clipster/Domain/Model/URLMetadata.swift
@@ -4,6 +4,7 @@ struct URLMetadata {
     let url: URL
     let title: String
     let thumbnailImageURL: URL?
+    let screenshotData: Data?
     let createdAt: Date
     let updatedAt: Date
     let deletedAt: Date?
