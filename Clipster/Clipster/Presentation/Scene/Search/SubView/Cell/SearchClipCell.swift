6def5f788015f4787c910a4111a23efb9f511b13
JISUNG PARK <meowbutlerdev@gmail.com>
2025-06-27 19:46:17 +0900

    feat: 클립 검색 결과 셀 구현

---
+++ Clipster/Clipster/Presentation/Scene/Search/SubView/Cell/SearchClipCell.swift
@@ -0,0 +1,44 @@
+import SnapKit
+import UIKit
+
+final class SearchClipCell: UICollectionViewCell {
+    private let searchClipRowView = SearchClipRowView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func setDisplay(_ clip: ClipDisplay, query: String) {
+        searchClipRowView.setDisplay(clip, query: query)
+    }
+}
+
+private extension SearchClipCell {
+    func configure() {
+        setAttributes()
+        setHierarchy()
+        setConstraints()
+    }
+
+    func setAttributes() {
+        contentView.backgroundColor = .white900
+        contentView.layer.cornerRadius = 12
+    }
+
+    func setHierarchy() {
+        [searchClipRowView]
+            .forEach { contentView.addSubview($0) }
+    }
+
+    func setConstraints() {
+        searchClipRowView.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(12)
+            make.directionalHorizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+}
