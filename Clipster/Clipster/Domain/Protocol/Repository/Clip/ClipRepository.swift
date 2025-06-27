92d08352c079021e1270c542190d971b416b6fc6
youseokhwan <youseokhwan15@gmail.com>
2025-06-27 13:25:06 +0900

    feat: ClipRepository fetchTopLevelClips() 구현

--- Clipster/Clipster/Domain/Protocol/Repository/Clip/ClipRepository.swift
+++ Clipster/Clipster/Domain/Protocol/Repository/Clip/ClipRepository.swift
@@ -2,6 +2,7 @@ import Foundation
 
 protocol ClipRepository {
     func fetchClip(by id: UUID) async -> Result<Clip, DomainError>
+    func fetchTopLevelClips() async -> Result<[Clip], DomainError>
     func fetchUnvisitedClips() async -> Result<[Clip], DomainError>
     func createClip(_ clip: Clip) async -> Result<Void, DomainError>
     func updateClip(_ clip: Clip) async -> Result<Void, DomainError>
