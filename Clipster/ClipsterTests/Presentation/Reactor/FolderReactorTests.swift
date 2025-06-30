8faff77c140039fee6a0015d051d69920a0e270b
youseokhwan <youseokhwan15@gmail.com>
2025-06-30 19:49:28 +0900

    test: FolderReactor 테스트 케이스 구분

--- Clipster/ClipsterTests/Presentation/Reactor/FolderReactorTests.swift
+++ Clipster/ClipsterTests/Presentation/Reactor/FolderReactorTests.swift
@@ -25,4 +25,68 @@ final class FolderReactorTests: XCTestCase {
         folder = nil
         super.tearDown()
     }
+
+    func test_viewWillAppear_최초진입() {
+
+    }
+
+    func test_viewWillAppear_이후진입() {
+
+    }
+
+    func test_폴더_셀_탭() {
+
+    }
+
+    func test_클립_셀_탭() {
+
+    }
+
+    func test_유효하지_않은_셀_탭() {
+
+    }
+
+    func test_폴더_추가_탭() {
+
+    }
+
+    func test_클립_추가_탭() {
+
+    }
+
+    func test_폴더_상세정보_탭() {
+        
+    }
+
+    func test_클립_상세정보_탭() {
+
+    }
+
+    func test_유효하지_않은_상세정보_탭() {
+
+    }
+
+    func test_폴더_편집_탭() {
+
+    }
+
+    func test_클립_편집_탭() {
+
+    }
+
+    func test_유효하지_않은_편집_탭() {
+
+    }
+
+    func test_폴더_삭제_탭() {
+
+    }
+
+    func test_클립_삭제_탭() {
+
+    }
+
+    func test_유효하지_않은_삭제_탭() {
+
+    }
 }
