90a31d452c9aeb1f88004983be8b5cf2054bc247
dbguswls030 <dbguswls030@gmail.com>
2025-07-01 19:29:17 +0900

    feat: fcm 연동

--- Clipster/Clipster/App/Source/AppDelegate.swift
+++ Clipster/Clipster/App/Source/AppDelegate.swift
@@ -1,4 +1,7 @@
+import FirebaseCore
+import FirebaseMessaging
 import UIKit
+import UserNotifications
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,7 +9,19 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?,
     ) -> Bool {
-        true
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in
+                print("")
+            }
+        )
+        FirebaseApp.configure()
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        return true
     }
 
     func application(
@@ -18,5 +33,37 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
             name: "Default Configuration",
             sessionRole: connectingSceneSession.role,
         )
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(
+        _ messaging: Messaging,
+        didReceiveRegistrationToken fcmToken: String?
+    ) {
+        print("\(Self.self) Firebase registration token: \(fcmToken ?? "")")
+        let dict = ["fcmToken": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dict
+        )
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.list, .banner, .sound])
     }
 }
