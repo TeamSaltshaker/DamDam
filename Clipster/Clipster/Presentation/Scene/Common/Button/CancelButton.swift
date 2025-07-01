a38e0d5dd3220435390cfc3b81979fb1919f4079
dbguswls030 <dbguswls030@gmail.com>
2025-07-01 20:51:34 +0900

    feat: 공통 컴포넌트 Cancel 버튼 구현

---
+++ Clipster/Clipster/Presentation/Scene/Common/Button/CancelButton.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+final class CancelButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension CancelButton {
+    func configure() {
+        setAttributes()
+    }
+
+    func setAttributes() {
+        let normalFont = UIFont.pretendard(size: 16, weight: .semiBold)
+
+        let normalAttributes: [NSAttributedString.Key: Any] = [
+            .font: normalFont,
+            .foregroundColor: UIColor.black100
+        ]
+
+        setAttributedTitle(NSAttributedString(string: "취소", attributes: normalAttributes), for: .normal)
+
+        contentHorizontalAlignment = .leading
+    }
+}
