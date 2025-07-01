b2678e43d80e745403e3e00cf04845e13a4ada52
JISUNG PARK <meowbutlerdev@gmail.com>
2025-07-02 00:13:54 +0900

    feat: 확인 버튼 추가

---
+++ Clipster/Clipster/Presentation/Scene/Common/Button/ConfirmButton.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+final class ConfirmButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+private extension ConfirmButton {
+    func configure() {
+        setAttributes()
+    }
+
+    func setAttributes() {
+        let normalFont = UIFont.pretendard(size: 16, weight: .semiBold)
+        let disabledFont = UIFont.pretendard(size: 16, weight: .medium)
+
+        let normalAttributes: [NSAttributedString.Key: Any] = [
+            .font: normalFont,
+            .foregroundColor: UIColor.blue400
+        ]
+        let disabledAttributes: [NSAttributedString.Key: Any] = [
+            .font: disabledFont,
+            .foregroundColor: UIColor.black800
+        ]
+
+        setAttributedTitle(NSAttributedString(string: "확인", attributes: normalAttributes), for: .normal)
+        setAttributedTitle(NSAttributedString(string: "확인", attributes: disabledAttributes), for: .disabled)
+
+        contentHorizontalAlignment = .trailing
+    }
+}
