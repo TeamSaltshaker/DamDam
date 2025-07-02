01e9e6989ddfbd72bb0023da1ad593562eeb8063
youseokhwan <youseokhwan15@gmail.com>
2025-07-02 20:12:41 +0900

    feat: DefaultAuthService 추가 기능 구현

--- Clipster/Clipster/Data/Network/Auth/DefaultAuthService.swift
+++ Clipster/Clipster/Data/Network/Auth/DefaultAuthService.swift
@@ -8,15 +8,46 @@ final class DefaultAuthService: AuthService {
         self.client = client
     }
 
+    func currentUserID() -> UUID? {
+        client.auth.currentUser?.id
+    }
+
     func loginWithApple(token: String) async -> Result<Void, Error> {
         let credentials = OpenIDConnectCredentials(provider: .apple, idToken: token)
 
         do {
             let response = try await client.auth.signInWithIdToken(credentials: credentials)
-            print("\(Self.self): ✅ Login Success. UID: \(response.user.id)")
+            print("\(Self.self): ✅ Login success. UID: \(response.user.id)")
             return .success(())
         } catch {
-            print("\(Self.self): ❌ \(error.localizedDescription)")
+            print("\(Self.self): ❌ Login With Apple Failed: \(error.localizedDescription)")
+            return .failure(error)
+        }
+    }
+
+    func logout() async -> Result<Void, Error> {
+        do {
+            try await client.auth.signOut()
+            print("\(Self.self): ✅ Logout success")
+            return .success(())
+        } catch {
+            print("\(Self.self): ❌ Logout Failed: \(error.localizedDescription)")
+            return .failure(error)
+        }
+    }
+
+    func withdraw() async -> Result<Void, Error> {
+        guard let user = client.auth.currentUser else {
+            print("\(Self.self): ❌ Withdraw Failed. Not Logged In.")
+            return .failure(AuthError.notLoggedIn)
+        }
+
+        do {
+            try await client.auth.admin.deleteUser(id: user.id)
+            print("\(Self.self): ✅ Withdraw success")
+            return .success(())
+        } catch {
+            print("\(Self.self): ❌ Withdraw Failed. \(error.localizedDescription)")
             return .failure(error)
         }
     }
--- Clipster/Clipster/Data/Protocol/Network/AuthService.swift
+++ Clipster/Clipster/Data/Protocol/Network/AuthService.swift
@@ -1,3 +1,8 @@
+import Foundation
+
 protocol AuthService {
+    func currentUserID() -> UUID?
     func loginWithApple(token: String) async -> Result<Void, Error>
+    func logout() async -> Result<Void, Error>
+    func withdraw() async -> Result<Void, Error>
 }
