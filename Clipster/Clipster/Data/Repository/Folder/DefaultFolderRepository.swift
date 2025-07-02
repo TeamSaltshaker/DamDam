e38cc166ad47ac8a7c1fbea3933701be374f2cd9
youseokhwan <youseokhwan15@gmail.com>
2025-07-02 17:06:08 +0900

    refactor: cache를 optional로 변경

--- Clipster/Clipster/Data/Repository/Folder/DefaultFolderRepository.swift
+++ Clipster/Clipster/Data/Repository/Folder/DefaultFolderRepository.swift
@@ -2,15 +2,16 @@ import Foundation
 
 final class DefaultFolderRepository: FolderRepository {
     private let storage: FolderStorage
-    private let cache: FolderClipCache
+    private let cache: FolderClipCache?
 
-    init(storage: FolderStorage, cache: FolderClipCache) {
+    init(storage: FolderStorage, cache: FolderClipCache?) {
         self.storage = storage
         self.cache = cache
     }
 
     func fetchFolder(by id: UUID) async -> Result<Folder, DomainError> {
-        if await cache.isFoldersInitialized {
+        if let cache,
+           await cache.isFoldersInitialized {
             guard let folder = await cache.folder(by: id) else {
                 return .failure(.entityNotFound)
             }
@@ -22,7 +23,8 @@ final class DefaultFolderRepository: FolderRepository {
     }
 
     func fetchTopLevelFolders() async -> Result<[Folder], DomainError> {
-        if await cache.isFoldersInitialized {
+        if let cache,
+           await cache.isFoldersInitialized {
             let folders = await cache.folders()
             return .success(folders.filter { $0.parentFolderID == nil && $0.deletedAt == nil })
         } else {
@@ -36,7 +38,8 @@ final class DefaultFolderRepository: FolderRepository {
 
         switch result {
         case .success:
-            if await cache.isFoldersInitialized {
+            if let cache,
+               await cache.isFoldersInitialized {
                 await cache.setFolder(folder)
             }
             return .success(())
@@ -50,7 +53,8 @@ final class DefaultFolderRepository: FolderRepository {
 
         switch result {
         case .success:
-            if await cache.isFoldersInitialized {
+            if let cache,
+               await cache.isFoldersInitialized {
                 await cache.setFolder(folder)
             }
             return .success(())
@@ -64,7 +68,8 @@ final class DefaultFolderRepository: FolderRepository {
 
         switch result {
         case .success:
-            if await cache.isFoldersInitialized {
+            if let cache,
+               await cache.isFoldersInitialized {
                 await cache.setFolder(folder)
             }
             return .success(())
