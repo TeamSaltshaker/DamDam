fb7ff4262814eb18c9cfeb6b070a63b972e2b304
JISUNG PARK <meowbutlerdev@gmail.com>
2025-07-02 13:43:43 +0900

    feat: 전체 클립 가져오는 Storage 구현

--- Clipster/Clipster/Data/Protocol/Persistence/ClipStorage.swift
+++ Clipster/Clipster/Data/Protocol/Persistence/ClipStorage.swift
@@ -2,6 +2,7 @@ import Foundation
 
 protocol ClipStorage {
     func fetchClip(by id: UUID) async -> Result<Clip, CoreDataError>
+    func fetchAllClips() async -> Result<[Clip], CoreDataError>
     func fetchTopLevelClips() async -> Result<[Clip], CoreDataError>
     func fetchUnvisitedClips() async -> Result<[Clip], CoreDataError>
     func insertClip(_ clip: Clip) async -> Result<Void, CoreDataError>
