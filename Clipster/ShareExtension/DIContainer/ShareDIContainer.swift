57960d5ad384d83c45b9f0703c9f981c7e4369e5
dbguswls030 <dbguswls030@gmail.com>
2025-07-02 20:49:54 +0900

    feat: reactor 추가 및 바인딩

---
+++ Clipster/ShareExtension/DIContainer/ShareDIContainer.swift
@@ -0,0 +1,66 @@
+import CoreData
+import Foundation
+
+final class ShareDIContainer {
+    private let container: NSPersistentContainer
+    let cache = FolderClipCache()
+    init() {
+        self.container = CoreDataStack.shared.container
+    }
+
+    func makeClipStorage() -> ClipStorage {
+        DefaultClipStorage(container: container, mapper: DomainMapper())
+    }
+
+    func makeURLRepository() -> DefaultURLRepository {
+        DefaultURLRepository()
+    }
+
+    func makeClipRepository() -> ClipRepository {
+        DefaultClipRepository(storage: makeClipStorage(), cache: cache)
+    }
+
+    func makeCreateClipUseCase() -> CreateClipUseCase {
+        DefaultCreateClipUseCase(clipRepository: makeClipRepository())
+    }
+
+    func makeParseURLUseCase() -> ParseURLUseCase {
+        DefaultParseURLUseCase(urlMetaRepository: makeURLRepository())
+    }
+
+    func makeShareReactor() -> ShareReactor {
+        ShareReactor(
+            parseURLUseCase: makeParseURLUseCase(),
+            createClipUseCase: makeCreateClipUseCase()
+        )
+    }
+
+    func makeFolderStorage() -> FolderStorage {
+        DefaultFolderStorage(container: container, mapper: DomainMapper())
+    }
+
+    func makeFolderRepository() -> FolderRepository {
+        DefaultFolderRepository(storage: makeFolderStorage(), cache: cache)
+    }
+
+    func makeFetchTopLevelFoldersUseCase() -> FetchTopLevelFoldersUseCase {
+        DefaultFetchTopLevelFoldersUseCase(folderRepository: makeFolderRepository())
+    }
+
+    func makeFindFolderPathUseCase() -> FindFolderPathUseCase {
+        DefaultFindFolderPathUseCase()
+    }
+
+    func makeFilterSubfoldersUseCase() -> FilterSubfoldersUseCase {
+        DefaultFilterSubfoldersUseCase()
+    }
+
+    func makeFolderSelectorReactorForClip(parentFolder: Folder?) -> FolderSelectorReactor {
+        FolderSelectorReactor(
+            fetchTopLevelFoldersUseCase: makeFetchTopLevelFoldersUseCase(),
+            findFolderPathUseCase: makeFindFolderPathUseCase(),
+            filterSubfoldersUseCase: makeFilterSubfoldersUseCase(),
+            parentFolder: parentFolder
+        )
+    }
+}
--- Clipster/ShareExtension/ViewController/ShareViewController.swift
+++ Clipster/ShareExtension/ViewController/ShareViewController.swift
@@ -1,6 +1,5 @@
 import ReactorKit
-import SnapKit
-import Social
+import RxCocoa
 import UIKit
 import UniformTypeIdentifiers
 
@@ -27,21 +26,27 @@ final class ShareViewController: UIViewController {
 
     private let shareView = ShareView()
 
-    override func loadView() {
-        view = shareView
-    }
-
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        self.reactor = ShareReactor()
+        self.reactor = ShareDIContainer().makeShareReactor()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func loadView() {
+        view = shareView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTappedBackground()
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        navigationController?.interactivePopGestureRecognizer?.delegate = self
         reactor?.action.onNext(.viewWillAppear)
     }
 
@@ -109,7 +114,88 @@ extension ShareViewController: View {
         bindState(from: reactor)
     }
 
-    private func bindUI(to reactor: ShareReactor) {}
+    private func bindUI(to reactor: ShareReactor) {
+        shareView.urlTextField
+            .rx
+            .controlEvent(.editingChanged)
+            .withLatestFrom(shareView.urlTextField.rx.text.orEmpty)
+            .distinctUntilChanged()
+            .map { Reactor.Action.editURLTextField($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.urlTextField
+            .rx
+            .controlEvent(.editingChanged)
+            .withLatestFrom(shareView.urlTextField.rx.text.orEmpty)
+            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+            .map { Reactor.Action.validifyURL($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.urlTextField
+            .rx
+            .controlEvent(.editingChanged)
+            .withLatestFrom(shareView.urlTextField.rx.text.orEmpty)
+            .distinctUntilChanged()
+            .map { _ in Reactor.Action.editingURLTextField }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.memoView.memoTextView
+            .rx
+            .text
+            .orEmpty
+            .distinctUntilChanged()
+            .map { Reactor.Action.editMemo($0) }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.memoView.memoTextView
+            .rx
+            .text
+            .orEmpty
+            .distinctUntilChanged()
+            .map { String($0.prefix(100)) }
+            .asDriver(onErrorJustReturn: "")
+            .drive(shareView.memoView.memoTextView.rx.text)
+            .disposed(by: disposeBag)
+
+        shareView.cancelButton
+            .rx
+            .tap
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] _ in
+                self?.close()
+            }
+            .disposed(by: disposeBag)
+
+        shareView.selectedFolderView.folderViewTapGesture
+            .rx
+            .event
+            .map { _ in Reactor.Action.tapFolderView }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.saveButton
+            .rx
+            .tap
+            .map { _ in Reactor.Action.saveClip }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        shareView.urlTextField.clearButton
+            .rx
+            .tap
+            .flatMap { _ in
+                Observable.of(
+                    Reactor.Action.editURLTextField(""),
+                    Reactor.Action.validifyURL("")
+                )
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
 
     private func bindState(from reactor: ShareReactor) {
         reactor.state
@@ -123,5 +209,163 @@ extension ShareViewController: View {
                 }
             }
             .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.urlString)
+            .filter { !$0.isEmpty }
+            .take(1)
+            .observe(on: MainScheduler.asyncInstance)
+            .flatMap { urlString in
+                Observable.of(
+                    .editingURLTextField,
+                    .editURLTextField(urlString),
+                    .validifyURL(urlString)
+                )
+            }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.urlString)
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: "")
+            .drive(shareView.urlTextField.rx.text)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.urlTextFieldBorderColor)
+            .map { UIColor(resource: $0).cgColor }
+            .asDriver(onErrorDriveWith: .empty())
+            .drive(shareView.urlTextField.layer.rx.borderColor)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isHiddenURLMetadataStackView)
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] isHidden in
+                self?.shareView.urlMetadataStackView.setHiddenAnimated(isHidden)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isHiddenURLValidationStackView)
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] isHidden in
+                self?.shareView.urlValidationStacKView.setHiddenAnimated(isHidden)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isLoading)
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] isLoading in
+                if isLoading {
+                    self?.shareView.urlValidationStacKView.activityIndicatorView.startAnimating()
+                } else {
+                    self?.shareView.urlValidationStacKView.activityIndicatorView.stopAnimating()
+                }
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.isSuccessedEditClip)
+            .filter { $0 }
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] _ in
+                self?.close()
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map { ($0.isTappedFolderView, $0.currentFolder) }
+            .filter { $0.0 }
+            .map { $0.1 }
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] currentFolder in
+                guard let self else { return }
+
+                let reactor = ShareDIContainer().makeFolderSelectorReactorForClip(parentFolder: currentFolder)
+                let vc = FolderSelectorViewController(reactor: reactor)
+                vc.onSelectionComplete = { [weak self] in
+                    self?.reactor?.action.onNext(.editFolder($0))
+                }
+                vc.modalPresentationStyle = .pageSheet
+                if let sheet = vc.sheetPresentationController {
+                    sheet.detents = [.custom { $0.maximumDetentValue * 0.75 }]
+                    sheet.prefersGrabberVisible = true
+                }
+                self.present(vc, animated: true)
+                self.reactor?.action.onNext(.disappearFolderSelectorView)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.currentFolder)
+            .distinctUntilChanged { $0?.id == $1?.id }
+            .asDriver(onErrorDriveWith: .empty())
+            .drive(with: self) { owner, folder in
+                let display = folder.map(FolderDisplayMapper.map)
+                owner.shareView.selectedFolderView.folderRowView.setDisplay(display)
+            }
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.memoLimit)
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: "0 / 100")
+            .drive(shareView.memoView.memoLimitLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map {
+                if let imageResource = $0.urlValidationImageResource {
+                    return UIImage(resource: imageResource)
+                } else {
+                    return .none
+                }
+            }
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: .none)
+            .drive(shareView.urlValidationStacKView.statusImageView.rx.image)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.urlValidationLabelText)
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive(shareView.urlValidationStacKView.statusLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        reactor.state
+            .map(\.urlMetadataDisplay)
+            .compactMap { $0 }
+            .distinctUntilChanged()
+            .asDriver(onErrorDriveWith: .empty())
+            .drive { [weak self] urlMetadataDisplay in
+                self?.shareView.urlMetadataStackView.setDisplay(display: urlMetadataDisplay)
+            }
+            .disposed(by: disposeBag)
+
+        Observable.combineLatest(
+            reactor.state.map(\.isURLValid),
+            reactor.state.map(\.isLoading)
+        )
+        .map { isURLValid, isLoading in
+            !isLoading && isURLValid
+        }
+        .distinctUntilChanged()
+        .asDriver(onErrorDriveWith: .empty())
+        .drive(shareView.saveButton.rx.isEnabled)
+        .disposed(by: disposeBag)
+    }
+}
+
+extension ShareViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
     }
 }
