c65539ad8e44da0b62d6fa27513e87fa783705df
dbguswls030 <dbguswls030@gmail.com>
2025-07-02 16:58:49 +0900

    refactor: Observable Extension 추가

--- Clipster/Clipster/Presentation/Scene/EditClip/Reactor/EditClipReactor.swift
+++ Clipster/Clipster/Presentation/Scene/EditClip/Reactor/EditClipReactor.swift
@@ -311,20 +311,3 @@ private extension EditClipReactor {
         )
     }
 }
-
-extension Observable {
-    static func fromAsync<T>(_ block: @escaping () async throws -> T) -> Observable<T> {
-        Single.create { emitter in
-            Task {
-                do {
-                    let result = try await block()
-                    emitter(.success(result))
-                } catch {
-                    emitter(.failure(error))
-                }
-            }
-            return Disposables.create()
-        }
-        .asObservable()
-    }
-}
