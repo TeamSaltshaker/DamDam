ef46749acd6b5f91fb3e9e9fe47bbfc532b43e90
JISUNG PARK <meowbutlerdev@gmail.com>
2025-07-02 15:32:50 +0900

    feat: ClipDisplay 날짜 추가

--- Clipster/Clipster/Presentation/Util/Mapper/ClipDisplayMapper.swift
+++ Clipster/Clipster/Presentation/Util/Mapper/ClipDisplayMapper.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 struct ClipDisplayMapper {
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 M월 d일"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter
+    }()
+
     static func map(_ clip: Clip) -> ClipDisplay {
         let urlMetadataDisplay = URLMetadataDisplay(
             url: clip.url,
@@ -10,12 +17,28 @@ struct ClipDisplayMapper {
             screenshotImageData: clip.screenshotData
         )
 
+        let recentVisitedDate = formatDateString(prefix: "최근 방문한 날짜:", date: clip.lastVisitedAt, fallback: "방문 이력 없음")
+        let recentEditedDate = formatDateString(prefix: "최근 수정한 날짜:", date: clip.updatedAt)
+        let createdDate = formatDateString(prefix: "생성된 날짜:", date: clip.createdAt)
+
         return ClipDisplay(
             id: clip.id,
             urlMetadata: urlMetadataDisplay,
             memo: clip.memo.isEmpty ? " " : clip.memo,
             memoLimit: "\(clip.memo.count) / 100",
             isVisited: clip.lastVisitedAt != nil,
+            recentVisitedDate: recentVisitedDate,
+            recentEditedDate: recentEditedDate,
+            createdDate: createdDate
         )
+    }
+
+    private static func formatDateString(prefix: String, date: Date?, fallback: String = "") -> String {
+        if let date {
+            let dateString = dateFormatter.string(from: date)
+            return "\(prefix) \(dateString)"
+        } else {
+            return "\(prefix) \(fallback)"
+        }
     }
 }
