7b7716b3b1d7150f668e1b522fe502e0403f65d2
JISUNG PARK <meowbutlerdev@gmail.com>
2025-07-03 10:39:57 +0900

    feat: 최근 검색어 삭제 로직 구현

--- Clipster/Clipster/Domain/Protocol/UseCase/Query/DeleteRecentQueryUseCase.swift
+++ Clipster/Clipster/Domain/Protocol/UseCase/Query/DeleteRecentQueryUseCase.swift
@@ -1,3 +1,3 @@
 protocol DeleteRecentQueryUseCase {
-    func execute(_ query: String) async -> Result<Void, Error>
+    func execute(_ query: String)
 }
--- Clipster/Clipster/Domain/UseCase/Query/DefaultDeleteRecentQueryUseCase.swift
+++ Clipster/Clipster/Domain/UseCase/Query/DefaultDeleteRecentQueryUseCase.swift
@@ -1,5 +1,17 @@
+import Foundation
+
 final class DefaultDeleteRecentQueryUseCase: DeleteRecentQueryUseCase {
-    func execute(_ query: String) async -> Result<Void, Error> {
-        .success(())
+    private let userDefaults: UserDefaults
+    private let key = "recentQueries"
+
+    init(userDefaults: UserDefaults) {
+        self.userDefaults = userDefaults
+    }
+
+    func execute(_ query: String) {
+        var queries = userDefaults.stringArray(forKey: key) ?? []
+        queries.removeAll { $0 == query }
+
+        userDefaults.set(queries, forKey: key)
     }
 }
