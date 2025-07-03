1a53ab43da048cccf6552c9f5aa2ee8a9286abe9
JISUNG PARK <meowbutlerdev@gmail.com>
2025-07-03 16:18:43 +0900

    test: FilterSubfoldersUseCaseTests 목 데이터 사용하도록 수정

--- Clipster/ClipsterTests/Domain/UseCase/Folder/FilterSubfoldersUseCaseTests.swift
+++ Clipster/ClipsterTests/Domain/UseCase/Folder/FilterSubfoldersUseCaseTests.swift
@@ -15,66 +15,38 @@ final class FilterSubfoldersUseCaseTests: XCTestCase {
     }
 
     func test_최상위폴더_목록에서_자기자신을_제외하고_반환() {
-        let top1 = makeFolder(id: "00000001-0000-0000-0000-000000000000", title: "Top 1", depth: 0)
-        let top2 = makeFolder(id: "00000002-0000-0000-0000-000000000000", title: "Top 2", depth: 0)
-        let top3 = makeFolder(id: "00000003-0000-0000-0000-000000000000", title: "Top 3", depth: 0)
-        let topLevel = [top1, top2, top3]
+        let tree = MockFolder.rootFolders + [MockFolder.someFolder]
+        let folder = MockFolder.someFolder
 
-        let result = useCase.execute(topLevelFolders: topLevel, currentPath: [], folder: top2)
+        let result = useCase.execute(folder, from: tree)
 
-        XCTAssertEqual(result.count, 2)
-        XCTAssertFalse(result.contains(where: { $0.id == top2.id }))
+        XCTAssertEqual(result.count, tree.count - 1)
+        XCTAssertFalse(result.contains(where: { $0.id == folder.id }))
     }
 
     func test_하위폴더_목록에서_자기자신을_제외하고_반환() {
-        let sub1 = makeFolder(id: "00000000-0001-0000-0000-000000000000", parentID: "00000001-0000-0000-0000-000000000000", title: "Sub 1", depth: 1)
-        let sub2 = makeFolder(id: "00000000-0002-0000-0000-000000000000", parentID: "00000001-0000-0000-0000-000000000000", title: "Sub 2", depth: 1)
-        let parent = makeFolder(id: "00000001-0000-0000-0000-000000000000", title: "Parent", depth: 0, folders: [sub1, sub2])
-        let topLevel = [parent]
+        let tree = [MockFolder.someFolder]
+        let folder = MockFolder.someFolder.folders.first!
 
-        let currentPath = [parent]
+        let result = useCase.execute(folder, from: tree)
 
-        let result = useCase.execute(topLevelFolders: topLevel, currentPath: currentPath, folder: sub2)
+        let resultRoot = result.first
+        let uikitExists = resultRoot?.folders.contains(where: { $0.id == folder.id }) ?? true
 
         XCTAssertEqual(result.count, 1)
-        XCTAssertEqual(result.first?.id, sub1.id)
+        XCTAssertFalse(uikitExists)
+        XCTAssertEqual(resultRoot?.folders.count, 1)
     }
 
-    func test_최상위폴더_목록에서_제외할_폴더가_nil이면_전체_폴더_목록_반환() {
-        let top1 = makeFolder(id: "00000001-0000-0000-0000-000000000000", title: "Top 1", depth: 0)
-        let top2 = makeFolder(id: "00000002-0000-0000-0000-000000000000", title: "Top 2", depth: 0)
-        let top3 = makeFolder(id: "00000003-0000-0000-0000-000000000000", title: "Top 3", depth: 0)
-        let topLevel = [top1, top2, top3]
+    func test_제외할_폴더가_없으면_전체_폴더_목록_반환() {
+        let folderTree = [MockFolder.someFolder]
+        let nonExistentFolder = MockFolder.folderToEdit
 
-        let result = useCase.execute(topLevelFolders: topLevel, currentPath: [], folder: nil)
+        let result = useCase.execute(nonExistentFolder, from: folderTree)
 
-        XCTAssertEqual(result.count, 3)
-        XCTAssertEqual(Set(result.map(\.id)), Set([top1.id, top2.id, top3.id]))
-    }
-
-    func test_하위폴더_목록에서_제외할_폴더가_nil이면_전체_폴더_목록_반환() {
-        let sub1 = makeFolder(id: "00000000-0001-0000-0000-000000000000", parentID: "00000001-0000-0000-0000-000000000000", title: "Sub 1", depth: 1)
-        let sub2 = makeFolder(id: "00000000-0002-0000-0000-000000000000", parentID: "00000001-0000-0000-0000-000000000000", title: "Sub 2", depth: 1)
-        let parent = makeFolder(id: "00000001-0000-0000-0000-000000000000", title: "Parent", depth: 0, folders: [sub1, sub2])
-        let topLevel = [parent]
-        let currentPath = [parent]
-
-        let result = useCase.execute(topLevelFolders: topLevel, currentPath: currentPath, folder: nil)
-
-        XCTAssertEqual(result.count, 2)
-        XCTAssertTrue(result.contains(where: { $0.id == sub1.id }))
-        XCTAssertTrue(result.contains(where: { $0.id == sub2.id }))
-    }
-
-    func test_현재폴더경로가_비어있고_제외할_폴더가_nil이면_최상위폴더_목록을_그대로_반환() {
-        let top1 = makeFolder(id: "00000001-0000-0000-0000-000000000000", title: "Top 1", depth: 0)
-        let top2 = makeFolder(id: "00000002-0000-0000-0000-000000000000", title: "Top 2", depth: 0)
-        let topLevel = [top1, top2]
-
-        let result = useCase.execute(topLevelFolders: topLevel, currentPath: [], folder: nil)
-
-        XCTAssertEqual(result.count, 2)
-        XCTAssertEqual(Set(result.map(\.id)), Set(topLevel.map(\.id)))
+        XCTAssertEqual(result.count, folderTree.count)
+        XCTAssertEqual(result.first?.id, folderTree.first?.id)
+        XCTAssertEqual(result.first?.folders.count, folderTree.first?.folders.count)
     }
 }
 
