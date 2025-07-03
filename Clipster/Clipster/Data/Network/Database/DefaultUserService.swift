fd96ac32949f77cc906e94737178760b7e5f12b4
youseokhwan <youseokhwan15@gmail.com>
2025-07-03 11:37:03 +0900

    feat: UserService fetch, insert, update 구현

--- Clipster/Clipster/Data/Network/Database/DefaultUserService.swift
+++ Clipster/Clipster/Data/Network/Database/DefaultUserService.swift
@@ -13,18 +13,57 @@ final class DefaultUserService: UserService {
         self.mapper = mapper
     }
 
-    func fetchUser(by id: UUID) async -> User? {
+    func fetchUser(by id: UUID) async -> Result<User, Error> {
         do {
             let dto: UserDTO = try await client
                 .from("Users")
                 .select()
                 .eq("id", value: id)
+                .single()
                 .execute()
                 .value
-            return mapper.user(from: dto)
+            let user = mapper.user(from: dto)
+            print("\(Self.self): ✅ Fetch Success. id: \(user.id), nickname: \(user.nickname)")
+            return .success(user)
         } catch {
             print("\(Self.self): ❌ Fetch Failed. \(error.localizedDescription)")
-            return nil
+            return .failure(error)
+        }
+    }
+
+    func insertUser(with id: UUID) async -> Result<Void, Error> {
+        do {
+            let dto = UserDTO(
+                id: id,
+                nickname: "김담담",
+                createdAt: Date.now,
+                updatedAt: Date.now,
+                deletedAt: nil,
+            )
+            try await client
+                .from("Users")
+                .insert(dto)
+                .execute()
+            print("\(Self.self): ✅ Insert Success. id: \(id)")
+            return .success(())
+        } catch {
+            print("\(Self.self): ❌ Insert Failed. \(error.localizedDescription)")
+            return .failure(error)
+        }
+    }
+
+    func updateUser(_ dto: UserDTO) async -> Result<Void, Error> {
+        do {
+            try await client
+                .from("Users")
+                .update(["nickname": dto.nickname])
+                .eq("id", value: dto.id)
+                .execute()
+            print("\(Self.self): ✅ Update Success. id: \(dto.id)")
+            return .success(())
+        } catch {
+            print("\(Self.self): ❌ Update Failed. \(error.localizedDescription)")
+            return .failure(error)
         }
     }
 }
--- Clipster/Clipster/Data/Protocol/Network/UserService.swift
+++ Clipster/Clipster/Data/Protocol/Network/UserService.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 protocol UserService {
-    func fetchUser(by id: UUID) async -> User?
+    func fetchUser(by id: UUID) async -> Result<User, Error>
+    func insertUser(with id: UUID) async -> Result<Void, Error>
+    func updateUser(_ dto: UserDTO) async -> Result<Void, Error>
 }
