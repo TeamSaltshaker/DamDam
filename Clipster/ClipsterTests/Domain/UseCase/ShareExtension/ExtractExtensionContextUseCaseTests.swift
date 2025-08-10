420d7e7dead900141ae15a60f777e81458108397
dbguswls030 <dbguswls030@gmail.com>
2025-08-10 16:48:55 +0900

    test: url 추출 성공 케이스 추가

--- Clipster/ClipsterTests/Domain/UseCase/ShareExtension/ExtractExtensionContextUseCaseTests.swift
+++ Clipster/ClipsterTests/Domain/UseCase/ShareExtension/ExtractExtensionContextUseCaseTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import UniformTypeIdentifiers
 @testable import Clipster
 
 final class ExtractExtensionContextUseCaseTests: XCTestCase {
@@ -12,5 +13,27 @@ final class ExtractExtensionContextUseCaseTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
         useCase = nil
+    }
+
+    func test_execute_URL_공유_시_URL_추출_성공() async {
+        let expectedURL = URL(string: "http://google.com")!
+        let mockProvider = NSItemProvider(
+            item: expectedURL as NSURL,
+            typeIdentifier: UTType.url.identifier
+        )
+
+        let extensionItem = NSExtensionItem()
+        extensionItem.attachments = [mockProvider]
+
+        let result = await useCase.execute(
+            extensionItems: [extensionItem]
+        )
+
+        switch result {
+        case .success(let url):
+            XCTAssertEqual(url, expectedURL)
+        case .failure:
+            XCTFail("URL 추출에 실패했습니다.")
+        }
     }
 }
