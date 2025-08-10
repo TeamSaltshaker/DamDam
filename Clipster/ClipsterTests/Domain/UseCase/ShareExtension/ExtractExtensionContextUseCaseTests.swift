af797f1d7d1d744d04fcd1265b64fa3e11026802
dbguswls030 <dbguswls030@gmail.com>
2025-08-10 17:13:02 +0900

    test: url 형태의 Data 케이스 추가

--- Clipster/ClipsterTests/Domain/UseCase/ShareExtension/ExtractExtensionContextUseCaseTests.swift
+++ Clipster/ClipsterTests/Domain/UseCase/ShareExtension/ExtractExtensionContextUseCaseTests.swift
@@ -58,4 +58,33 @@ final class ExtractExtensionContextUseCaseTests: XCTestCase {
             XCTFail("URL 추출에 실패했습니다.")
         }
     }
+
+    func test_execute_URL_형태의_Data_공유_시_URL_추출_성공() async {
+        let receivedData = "현재 페이지의 URL은 http://google.com 입니다."
+        let expectedURL = URL(string: "http://google.com")
+
+        guard let urlData = receivedData.data(using: .utf8) else {
+            XCTFail("테스트 데이터 생성에 실패했습니다.")
+            return
+        }
+
+        let mockProvider = NSItemProvider(
+            item: urlData as NSData,
+            typeIdentifier: UTType.data.identifier
+        )
+
+        let extensionItem = NSExtensionItem()
+        extensionItem.attachments = [mockProvider]
+
+        let result = await useCase.execute(
+            extensionItems: [extensionItem]
+        )
+
+        switch result {
+        case .success(let url):
+            XCTAssertEqual(url, expectedURL)
+        case .failure:
+            XCTFail("URL 추출에 실패했습니다.")
+        }
+    }
 }
